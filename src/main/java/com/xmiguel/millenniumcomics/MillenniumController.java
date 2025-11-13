bbb16d628c13f90c1409682602f4431e8003b911
xmiguelcastillo <xmiguelcastillo@gmail.com>
2025-11-12 23:32:33 -0500

    Update Supabase config

--- src/main/java/com/xmiguel/millenniumcomics/MillenniumController.java
+++ src/main/java/com/xmiguel/millenniumcomics/MillenniumController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin
 @RestController
 public class MillenniumController {
         private final ComicService comicService;
